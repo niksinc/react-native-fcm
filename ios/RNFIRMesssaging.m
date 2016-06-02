bd113888f5482b7f4d8a8a8ae43474dc86474126
Vani-gurnani <sunny.vanita@gmail.com>
2016-06-02 14:24:22 -0700

    Added Upload to FCM/Storage

--- ios/RNFIRMesssaging.m
+++ ios/RNFIRMesssaging.m
@@ -91,6 +91,47 @@ RCT_REMAP_METHOD(getFCMToken,
                                                    body:info];
 }
 
+RCT_REMAP_METHOD(UploadFileToFirebase,
+                 : (NSString*) localFile
+                 : (NSString*) contentType
+                 : (NSString*) bucket
+                 : (NSString*) key
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject){
+
+  FIRStorage *storage = [FIRStorage storage];
+  // Create a storage reference from our storage service
+  FIRStorageReference *storageRef = [storage referenceForURL:bucket];
+
+  
+  // Create a reference to the file you want to upload
+  //NSString *storageLocation = [@"images/" stringByAppendingString:key];
+  FIRStorageReference *riversRef = [storageRef child:key];
+  
+  // Upload the file to the path "images/rivers.jpg"
+  FIRStorageUploadTask *uploadTask = [riversRef putFile:localFile metadata:nil completion:^(FIRStorageMetadata *metadata, NSError *error) {
+    if (error != nil) {
+      // Uh-oh, an error occurred!
+      NSLog(@"Error in Uploading File to Firebase", error);
+      reject(@"Error", @"Failed upload file to firebase", error);
+    } else {
+      // Metadata contains file metadata such as size, content-type, and download URL.
+      NSURL *downloadURL = metadata.downloadURL;
+      NSLog(@"Successfully uploaded File to Firebase", metadata);
+      resolve(@ {@"downloadURL": downloadURL});
+    }
+  }];
+  
+  FIRStorageHandle observer = [uploadTask observeStatus:FIRStorageTaskStatusProgress
+                                                handler:^(FIRStorageTaskSnapshot *snapshot) {
+                                                  
+                                                  float progress = (float) snapshot.progress.totalUnitCount / snapshot.progress.completedUnitCount;
+                                                  [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithFloat:progress], @"key": key}];
+                                                  
+                                                }];
+  
+}
+
 RCT_EXPORT_METHOD(requestPermissions)
 {
     if (RCTRunningInAppExtension()) {
