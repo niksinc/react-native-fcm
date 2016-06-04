51d9d2acf617e7563d1d234bc2494c309f52e302
Vani-gurnani <sunny.vanita@gmail.com>
2016-06-04 04:35:14 -0700

    Added Firebase Storage Upload

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
@@ -1,8 +1,15 @@
 package com.evollu.react.fcm;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
@@ -12,18 +19,27 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import android.util.Log;
-
-import android.content.Context;
-
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
 public class FIRMessagingModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     private final static String TAG = FIRMessagingModule.class.getCanonicalName();
+    private StorageReference mStorageRef;
+    private Uri mDownloadUrl = null;
+    private Uri mFileUri = null;
+    //private FirebaseAuth mAuth;
 
     public FIRMessagingModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -46,6 +62,8 @@ public class FIRMessagingModule extends ReactContextBaseJavaModule implements Li
     @ReactMethod
     public void getFCMToken(Promise promise) {
         Log.d(TAG, "Firebase token: " + FirebaseInstanceId.getInstance().getToken());
+
+
         promise.resolve(FirebaseInstanceId.getInstance().getToken());
     }
 
@@ -53,7 +71,61 @@ public class FIRMessagingModule extends ReactContextBaseJavaModule implements Li
     getReactApplicationContext()
         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
         .emit(eventName, params);
+
     }
+
+    private Bitmap getBitmapFromUri(Uri fileUri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                this.getReactApplicationContext().getContentResolver().openFileDescriptor(fileUri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
+    @ReactMethod
+    public void UploadFileToFirebase(String localFile, String contentType, String bucket, String key, final Promise promise) throws IOException {
+
+
+        Uri fileUri = Uri.parse(localFile);
+
+          Bitmap bitmap =  getBitmapFromUri(fileUri);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+        byte[] data = baos.toByteArray();
+
+
+            mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl(bucket);
+            StorageReference photoRef  = mStorageRef.child(key);
+        UploadTask uploadTask = photoRef.putBytes(data);
+        // Observe state change events such as progress, pause, and resume
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                float progress = (taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                sendEvent("FirebaseUploadProgressChanged", progress);
+                System.out.println("Upload is " + progress + "% done");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                promise.reject(exception.toString());
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                promise.resolve(downloadUrl.toString());
+            }
+        });
+
+
+//
+   }
+   // [END upload_from_uri]
+
 
     private void registerTokenRefreshHandler() {
         IntentFilter intentFilter = new IntentFilter("com.evollu.react.fcm.FCMRefreshToken");
